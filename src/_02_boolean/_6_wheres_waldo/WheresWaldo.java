711a51e0934df1ea9a752934144fc4751fa9b279
league <league@imac-18.attlocal.net>
2023-02-19 11:31:56 -0800

    Waldo and words

--- src/_02_boolean/_6_wheres_waldo/WheresWaldo.java
+++ src/_02_boolean/_6_wheres_waldo/WheresWaldo.java
@@ -38,8 +38,8 @@ import processing.core.PImage;
  *      playWhoohoo() method, otherwise call the playDoh() method.
  */
 public class WheresWaldo extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
+    static final int WIDTH = 1070;
+    static final int HEIGHT = 719;
 
     PImage waldo;
 
@@ -51,12 +51,24 @@ public class WheresWaldo extends PApplet {
     @Override
     public void setup() {
         waldo = loadImage("waldo.jpg");
+        image(waldo,0,0);
+        waldo.resize(1070,719);
      
     }
 
     @Override
     public void draw() {
-        
+    	 println("X: " + mouseX + " Y: " + mouseY);
+    	 if(mousePressed) {
+    		 if(mouseX >= 661 && mouseY >= 359 && mouseX <= 754 && mouseY <= 505) {
+    				 playWhoohoo();
+    			 }else {
+    				 playDoh();
+    			 }
+    		 
+    		 
+    		
+    	 }
     }
 
     static public void main(String[] args) {
--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -1,18 +1,23 @@
 package _03_char._2_string_methods;
 
+import java.awt.print.Printable;
 
 public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+String sentence = "Words";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
-		
+System.out.println(sentence.charAt(2));
+System.out.println(sentence.length());		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+for (int i = 0; i < sentence.length(); i++) {
+	System.out.println(sentence.charAt(i));
+	
+	
+}
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
